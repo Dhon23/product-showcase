189a06ec127f85321e8a99a5579491a6193e4096
Dhon23 <m.ramadhan.ak@gmail.com>
2022-10-11 14:50:26 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BsShare } from "react-icons/bs";
+import { MdOutlineAddShoppingCart } from "react-icons/md";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [products, setProducts] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then(({ data }) => {
+        setProducts(data.products);
+      })
+      .catch(console.log)
+      .finally(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    console.log(products);
+  }, [products]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    !loading && (
+      <div className="cards">
+        {products.map((el) => (
+          <div key={el.id} className="card">
+            <div className="img">
+              <img src={el.thumbnail} alt="" />
+            </div>
+            <div className="caption">
+              <h3>{el.title}</h3>
+              <p>{el.description}</p>
+              <span>
+                Price: <strong>${el.price}</strong>
+              </span>
+            </div>
+            <div className="footer">
+              <div className="btn-add-cart">
+                <MdOutlineAddShoppingCart />
+                <h5>Add To Cart</h5>
+              </div>
+              <div className="btn-share">
+                <BsShare />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
   );
 }
-
-export default App;
